7a9d87160ad4a000b926d6b64c65ad4b1eddee15
wli118 <liw0118@gmail.com>
2018-04-24 22:04:31 -0700

    treetodoublylist

---
+++ tree/BB_005_Tree_To_Doubly_Linkedlist.java
@@ -0,0 +1,29 @@
+package tree;
+
+/**
+ * 
+ * BB.Tree.005 Tree To Doubly Linked List
+ * 
+ * Question : Given a tree, write a function to convert it into a circular
+ * doubly linked list from left to right by only modifying the existing
+ * pointers.
+ *
+ * e.g.
+ * 
+ * 1
+ * 
+ * 2 3
+ * 
+ * 4 5 6 7
+ * 
+ * output : <- 4 <-> 2 <-> 5 <-> 1 <-> 6 <-> 3 <-> 7 ->
+ * 
+ */
+public class BB_005_Tree_To_Doubly_Linkedlist {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

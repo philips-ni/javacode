40a9c4f55c3b87ae7bb2068a3f0b034272b18add
wli118 <liw0118@gmail.com>
2018-04-24 22:38:40 -0700

    treetodoublylist

--- common/Helper.java
+++ common/Helper.java
@@ -1,6 +1,7 @@
 package common;
 
 import foundation.LinkedListNode;
+import foundation.BTNode;
 
 public class Helper {
 	public static void printLinkedList(LinkedListNode n) {
--- tree/BB_005_Tree_To_Doubly_Linkedlist.java
+++ tree/BB_005_Tree_To_Doubly_Linkedlist.java
@@ -1,5 +1,7 @@
 package tree;
 
+import foundation.BTNode;
+
 /**
  * 
  * BB.Tree.005 Tree To Doubly Linked List
@@ -21,8 +23,64 @@ package tree;
  */
 public class BB_005_Tree_To_Doubly_Linkedlist {
 
+	private static BTNode concatenate(BTNode a, BTNode b) {
+	    if (a == null) return b;
+	    if (b == null) return a;
+
+	    BTNode aEnd = a.left;
+	    BTNode bEnd = b.left;
+
+	    a.left = bEnd;
+	    bEnd.right = a;
+	    aEnd.right = b;
+	    b.left = aEnd;
+	    return a;
+	}
+
+	public static BTNode treeToList(BTNode n) {
+	    if (n == null) return n;
+	    BTNode leftList = treeToList(n.left);
+	    BTNode rightList = treeToList(n.right);
+	    n.left = n;
+	    n.right = n;
+
+	    n = concatenate(leftList, n);
+	    n = concatenate(n, rightList);
+	    return n;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BTNode root = new BTNode();
+		root.val = 1;
+		
+		BTNode n1 = new BTNode();
+		n1.val = 2;
+		
+		BTNode n2 = new BTNode();
+		n2.val = 3;
+		
+		root.left = n1;
+		root.right = n2;
+		
+		BTNode n3 = new BTNode();
+		n3.val = 4;
+		
+		BTNode n4 = new BTNode();
+		n4.val = 5;
+		
+		n1.left=n3;
+		n1.right=n4;
+		
+		BTNode n5 = new BTNode();
+		n5.val=6;
+		
+		BTNode n6 = new BTNode();
+		n6.val=7;
+		
+		n2.left=n5;
+		n2.right=n6;
+		
+		treeToList(root);
 
 	}
 

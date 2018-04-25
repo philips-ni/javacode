878ae9896e78348a3e04a622376985ecb99dad45
wli118 <13264444+wli118@users.noreply.github.com>
2018-04-24 17:05:51 -0700

    palindrome

---
+++ stack/BB_006_Palindromes.java
@@ -0,0 +1,60 @@
+package stack;
+
+import java.util.Stack;
+import foundation.LinkedListNode;
+
+/**
+ * BB.Stack.006 Palindromes
+ * 
+ * Question : Given a linked list, write a function to determine whether the
+ * list is a palindrome.
+ *
+ * e.g.
+ * 
+ * palindrome(1 -> 2 -> 3) = false
+ * 
+ * palindrome(1 -> 2 -> 1) = true
+ */
+public class BB_006_Palindromes {
+
+	public static boolean palindrome(LinkedListNode node) {
+		Stack<Integer> st = new Stack<Integer>();
+
+		LinkedListNode cur = node;
+		LinkedListNode runner = node;
+
+		while (runner != null && runner.next != null) {
+			st.push(cur.value);
+			cur = cur.next;
+			runner = runner.next.next;
+		}
+
+		if (runner != null) {
+			// odd number list
+			cur = cur.next;
+		}
+
+		while (cur != null) {
+			if (cur.value != st.pop())
+				return false;
+			cur = cur.next;
+		}
+
+		return true;
+	}
+
+	public static void main(String[] args) {
+		LinkedListNode n1 = new LinkedListNode(1);
+		LinkedListNode n2 = new LinkedListNode(2);
+		LinkedListNode n3 = new LinkedListNode(2);
+		LinkedListNode n4 = new LinkedListNode(1);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = null;
+
+		System.out.println(palindrome(n1));
+
+	}
+
+}

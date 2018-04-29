9a989532c7c74015ba5cac777520b84d5a62b322
wli118 <13264444+wli118@users.noreply.github.com>
2018-04-29 10:42:22 -0700

    dp

---
+++ dynamicprogramming/BB_003_Square_Submatrix.java
@@ -0,0 +1,31 @@
+package dynamicprogramming;
+
+/**
+ * BB.Array.007 Square SubMatrix
+ * 
+ * Question: Given a 2D array of 1s and 0s, find the largest square subarray of
+ * all 1s.
+ * 
+ * e.g.
+ * 
+ * Input:
+ * 
+ * [1, 1, 1, 0]
+ * 
+ * [1, 1, 1, 1]
+ * 
+ * [1, 1, 0, 0]
+ * 
+ * Output:
+ * 
+ * 2
+ * 
+ */
+public class BB_003_Square_Submatrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dynamicprogramming/BB_005_Longest_Common_Substring.java
@@ -0,0 +1,58 @@
+package dynamicprogramming;
+
+/**
+ * BB.String.003 Longest common substring
+ * 
+ * Question : Given two strings, write a function that returns the longest
+ * common substring.
+ * 
+ * e.g.
+ * 
+ * longestSubstring("ABAB", "BABA") = "ABA"
+ * 
+ * 
+ *    substring length cache:
+ *    
+ *      A  B  A  B
+ *    B 0  1  0  1 
+ *    A 1  0  2  0
+ *    B 0  2  0  3 
+ *    A 1  0  3  0
+ *    
+ *    
+ */
+public class BB_005_Longest_Common_Substring {
+
+	public static String longestSubstring(String a, String b) {
+		String c = "";
+		int len = 0;
+		if (a.length() == 0 || b.length() == 0)
+			return c;
+		
+		int[][] cache = new int[a.length()][b.length()];
+
+		for (int i = 0; i < a.length(); i++) {
+			for (int j = 0; j < b.length(); j++) {
+
+				if (a.charAt(i) == b.charAt(j)) {
+					if (i == 0 || j == 0)
+						cache[i][j] = 1;
+					else
+						cache[i][j] = cache[i - 1][j - 1] + 1;
+
+					if (cache[i][j] > len) {	
+						len = cache[i][j];
+						c = a.substring(i - len + 1, i + 1);
+					}
+				}
+			}
+		}
+
+		return c;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(longestSubstring("ABAB","BABA"));
+	}
+
+}

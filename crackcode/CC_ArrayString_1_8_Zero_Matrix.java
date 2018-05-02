fb981473366ee2764d2fab603857a7eedc5e0278
wli118 <liw0118@gmail.com>
2018-05-01 21:46:48 -0700

    LinkedList

---
+++ crackcode/CC_ArrayString_1_8_Zero_Matrix.java
@@ -0,0 +1,146 @@
+package crackcode;
+
+/**
+ * CC.ArrayString.1.8 Zero Matrix
+ * 
+ * Question : Given a boolean matrix, update it so that if any cell is true, all
+ * the cells in that row and column are true.
+ * 
+ * Question : Write an algorithm such that if an element in an MxN matrix is 0,
+ * its entire row and column are set to 0.
+ * 
+ * e.g.
+ * 
+ * Input:
+ * 
+ * [true, false, false]
+ * 
+ * [false, false, false]
+ * 
+ * [false, false, true]
+ * 
+ * Output:
+ * 
+ * [true, true, true]
+ * 
+ * [true, false, true]
+ * 
+ * [true, true, true]
+ * 
+ */
+public class CC_ArrayString_1_8_Zero_Matrix {
+
+	public static void zeroMatrix(boolean[][] matrix) {
+
+		// rows and cols are used to track which rows and cols should be set to
+		// true. boolean type is default to false
+		boolean[] rows = new boolean[matrix.length];
+		boolean[] cols = new boolean[matrix[0].length];
+
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j]) {
+					rows[i] = true; // row i will be set to true
+					cols[j] = true; // col j will be set to true
+				}
+			}
+		}
+
+		// set rows to true
+		for (int i = 0; i < rows.length; i++) {
+			if (rows[i]) {
+				for (int j = 0; j < cols.length; j++) {
+					matrix[i][j] = true;
+				}
+			}
+		}
+
+		// set cols to true
+		for (int i = 0; i < cols.length; i++) {
+			if (cols[i]) {
+				for (int j = 0; j < rows.length; j++) {
+					matrix[j][i] = true;
+				}
+			}
+		}
+	}
+
+	public static void zeroMatrix_optimal(boolean[][] matrix) {
+
+		boolean zeroCol = false;
+		boolean zeroRow = false;
+
+		// get the value first column should be set
+		for (int i = 0; i < matrix.length; i++) {
+			zeroCol |= matrix[i][0];
+		}
+		// get the value first row should be set
+		for (int j = 0; j < matrix[0].length; j++) {
+			zeroRow |= matrix[0][j];
+		}
+
+		// check remaining inner matrix and track the row/column value with
+		// first
+		// column/row space
+		for (int i = 1; i < matrix.length; i++) {
+			for (int j = 1; j < matrix[0].length; j++) {
+				if (matrix[i][j]) {
+					matrix[i][0] = true;
+					matrix[0][j] = true;
+				}
+			}
+		}
+
+		// set rows
+		for (int i = 1; i < matrix.length; i++) {
+			if (matrix[i][0]) {
+				for (int j = 1; j < matrix[0].length; j++) {
+					matrix[i][j] = true;
+				}
+			}
+		}
+
+		// set columns
+		for (int j = 1; j < matrix[0].length; j++) {
+			if (matrix[0][j]) {
+				for (int i = 1; i < matrix.length; i++) {
+					matrix[i][j] = true;
+				}
+			}
+		}
+
+		// set first row
+		if (zeroRow) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				matrix[0][j] = true;
+			}
+		}
+
+		// set first column
+		if (zeroCol) {
+			for (int i = 0; i < matrix.length; i++) {
+				matrix[i][0] = true;
+			}
+		}
+	}
+
+	public static void printArray(boolean[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[0].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println("");
+		}
+
+	}
+
+	public static void main(String[] args) {
+		boolean[][] zeroMatrix = { { true, false, false }, { false, false, false }, { false, false, true } };
+		zeroMatrix_optimal(zeroMatrix);
+		printArray(zeroMatrix);
+		boolean[][] zeroMatrix1 = { { true, false, false }, { false, false, false }, { false, false, true } };
+		zeroMatrix_optimal(zeroMatrix1);
+		printArray(zeroMatrix1);
+	}
+
+}
---
+++ crackcode/CC_LinkedList_2_1_Remove_Dups.java
@@ -0,0 +1,61 @@
+package crackcode;
+
+import foundation.LinkedListNode;
+import java.util.HashSet;
+
+/**
+ * CC.LinkedList.2.1 Remove Dups
+ * 
+ * Question : Write code to remove duplicates from an unsorted linked list. How
+ * would you solve this problem if a temporary buffer is not allowed?
+ *
+ */
+public class CC_LinkedList_2_1_Remove_Dups {
+
+	public static void removeDups(LinkedListNode n) {
+		HashSet<Integer> hs = new HashSet<Integer>();
+		LinkedListNode prev = null;
+		while (n != null) {
+			if (hs.contains(n.value)) {
+				// found duplicate
+				prev.next = n.next;
+			} else {
+				hs.add(n.value);
+				prev = n;
+			}
+			n = n.next;
+		}
+	}
+
+	public static void removeDups_BF(LinkedListNode n) {
+		LinkedListNode current = n;
+
+		while (current != null) {
+			LinkedListNode runner = current;
+			while (runner.next != null) {
+				if (runner.next.value == current.value) {
+					// found duplicate
+					runner.next = runner.next.next;
+				} else {
+					runner = runner.next;
+				}
+			}
+			current = current.next;
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedListNode n1 = new LinkedListNode(1);
+		LinkedListNode n2 = new LinkedListNode(2);
+		LinkedListNode n3 = new LinkedListNode(3);
+		LinkedListNode n4 = new LinkedListNode(2);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
+		n4.next = null;
+
+		removeDups(n1);
+
+	}
+
+}

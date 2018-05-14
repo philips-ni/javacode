e0e9325d3f657757f430bef8e7069d6ddc05bb08
wli118 <13264444+wli118@users.noreply.github.com>
2018-05-14 11:18:20 -0700

    check subtree

--- crackcode/CC_TreeGraph_4_10_Check_Subtree.java
+++ crackcode/CC_TreeGraph_4_10_Check_Subtree.java
@@ -22,7 +22,7 @@ public class CC_TreeGraph_4_10_Check_Subtree {
 		getOrderString(n1, s1);
 		getOrderString(n2, s2);
 
-		return s1.indexOf(s2.toString()) != -1; 
+		return s1.indexOf(s2.toString()) != -1;
 
 	}
 
@@ -36,6 +36,36 @@ public class CC_TreeGraph_4_10_Check_Subtree {
 		sb.append(n.val);
 		getOrderString(n.left, sb);
 		getOrderString(n.right, sb);
+	}
+
+	public static boolean containsTree_Recursive(BTNode n1, BTNode n2) {
+		if (n2 == null)
+			return true; // empty tree is always a subtree
+
+		else
+			return subTree(n1, n2);
+	}
+
+	private static boolean subTree(BTNode n1, BTNode n2) {
+		if (n1 == null)
+			return false; // not found yet
+
+		if (n1.val == n2.val)
+			return matchTree(n1, n2); // check if current root contains target subtree
+		return subTree(n1.left, n2) || subTree(n1.right, n2); // check left and right
+	}
+
+	private static boolean matchTree(BTNode n1, BTNode n2) {
+		if (n2 == null)
+			return true; // all the nodes in subtree n2 have been found in n1. Match !
+
+		if (n1 == null)
+			return false; // n1 is already empty while n2 is not. Not match !
+
+		if (n1.val != n2.val)
+			return false; // Not match in current node
+
+		return (matchTree(n1.left, n2.left) && matchTree(n1.right, n2.right));
 	}
 
 	public static void main(String[] args) {

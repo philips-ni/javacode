51da4433c92f3631d245e13eb5960e36ade6dedb
wli118 <liw0118@gmail.com>
2018-05-14 22:59:45 -0700

    robot in grid

--- crackcode/CC_Recursion_6_2_Robot_In_Grid.java
+++ crackcode/CC_Recursion_6_2_Robot_In_Grid.java
@@ -3,16 +3,16 @@ package crackcode;
 import java.util.ArrayList;
 
 /**
- * CC.Recursion.6.2 NxN paths
+ * CC.Recursion.8.2 Robot In a Grid
  * 
- * Question : Imagine a robot sitting on the upper left hand corner of an NxN
- * grid. The robot can only move in two directions: right and down. How many
- * possible paths are there for the robot? FOLLOW UP Imagine certain squares are
- * “off limits”, such that the robot can not step on them. Design an algorithm
- * to get all possible paths for the robot.
+ * Question : Imagine a robot sitting on the upper left hand corner with r rows
+ * and c columns. The robot can only move in two directions, right and down, but
+ * certain cells are "off limits" such that the robot cannot step on them.
+ * Design an algorithm to find a path for the robot from the top left to the
+ * bottom right.
  *
  */
-public class CC_Recursion_6_2_N_N_Paths {
+public class CC_Recursion_6_2_Robot_In_Grid {
 
 	private class Point {
 		int x;
@@ -75,7 +75,7 @@ public class CC_Recursion_6_2_N_N_Paths {
 	}
 
 	public static void main(String[] args) {
-		CC_Recursion_6_2_N_N_Paths test = new CC_Recursion_6_2_N_N_Paths();
+		CC_Recursion_6_2_Robot_In_Grid test = new CC_Recursion_6_2_Robot_In_Grid();
 		System.out.println(test.getPath(4, 4));
 		test.printPath();
 	}

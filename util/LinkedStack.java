a8faf265161b4de93c6ea2c06b8e91f00fa03a66
wli118 <13264444+wli118@users.noreply.github.com>
2018-05-17 13:42:53 -0700

    linked stack

--- util/LinkedStack.java
+++ util/LinkedStack.java
@@ -1,40 +1,45 @@
 package util;
 
+import java.util.EmptyStackException;
+
 public class LinkedStack<T> {
-	private static class StackNode<T>{
+	private static class StackNode<T> {
 		private T data;
 		private StackNode<T> next;
-		public StackNode(T data){
+
+		public StackNode(T data) {
 			this.data = data;
 		}
 	}
-	
+
 	private StackNode<T> top;
-	
-	public LinkedStack(){
+
+	public LinkedStack() {
 		top = null;
 	}
-	
-	public T pop() throws Exception{
-		if(top == null) throw new Exception();
+
+	public T pop() {
+		if (top == null)
+			throw new EmptyStackException();
 		T data = top.data;
 		top = top.next;
-		
+
 		return data;
 	}
-	
-	public void push(T data){
+
+	public void push(T data) {
 		StackNode<T> node = new StackNode<T>(data);
 		node.next = top;
 		top = node;
 	}
-	
-	public T peek() throws Exception{
-		if(top == null) throw new Exception();
-		return top.data;	
+
+	public T peek() {
+		if (top == null)
+			throw new EmptyStackException();
+		return top.data;
 	}
-	
-	public boolean isEmpty(){
-		return (top==null);
+
+	public boolean isEmpty() {
+		return (top == null);
 	}
 }

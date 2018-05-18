69a2b86a3dc4f68af60ed5b4130cb9ae53b8b452
wli118 <13264444+wli118@users.noreply.github.com>
2018-05-18 12:00:05 -0700

    quick sort

--- util/QuickSort.java
+++ util/QuickSort.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class QuickSort {
 	/**
 	 * Like Merge Sort, QuickSort is a Divide and Conquer algorithm. It picks an
-	 * element as pivot and partitions the given arr around the picked pivot. There
-	 * are many different versions of quickSort that pick pivot in different ways.
+	 * element as pivot and partitions the given array around the picked pivot.
+	 * There are many different versions of quickSort that pick pivot in different
+	 * ways.
 	 * 
 	 * 1. Always pick first element as pivot.
 	 * 
@@ -14,42 +15,30 @@ public class QuickSort {
 	 * 
 	 * 3. Pick a random element as pivot.
 	 * 
-	 * 4.Pick median as pivot.
+	 * 4. Pick median as pivot.
 	 * 
 	 * The key process in quickSort is partition(). Target of partitions is, given
-	 * an arr and an element x of arr as pivot, put x at its correct position in
-	 * sorted arr and put all smaller elements (smaller than x) before x, and put
+	 * an array and an element x of array as pivot, put x at its correct position in
+	 * sorted array and put all smaller elements (smaller than x) before x, and put
 	 * all greater elements (greater than x) after x. All this should be done in
 	 * linear time.
 	 * 
 	 */
-
-	/*
-	 * This function takes last element as pivot, places the pivot element at its
-	 * correct position in sorted arr, and places all smaller (smaller than pivot)
-	 * to left of pivot and all greater elements to right of pivot
-	 */
 	private static int partition(int arr[], int low, int high) {
-		int pivot = arr[high];
+		int pivot = arr[high]; // pick last element as pivot
+		
 		int i = (low - 1); // index of smaller element, start with -1
 		for (int j = low; j < high; j++) {
-
 			if (arr[j] <= pivot) {
-				// found element smaller than pivot
-				i++;
-
-				// swap arr[i] and arr[j]
-				// place smaller elements to the very left side
+				// place smaller elements to the very left side		
+				i++;		
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
-				System.out.println("after swap : " + Arrays.toString(arr));
 			}
 		}
 
-		// swap arr[i+1] and arr[high] (or pivot).
-		// i is pointing to very last position of element smaller than pivot
-		// i+1 is the final position for pivot
+		// move pivot to the right place
 		int temp = arr[i + 1];
 		arr[i + 1] = arr[high];
 		arr[high] = temp;
@@ -57,27 +46,15 @@ public class QuickSort {
 		return i + 1;
 	}
 
-	/*
-	 * The main function that implements QuickSort() arr[] --> arr to be sorted, low
-	 * --> Starting index, high --> Ending index
-	 */
 	public static void quickSort(int arr[], int low, int high) {
 		if (low < high) {
-			/*
-			 * pi is partitioning index, arr[pi] is now at right place
-			 */
-			int pi = partition(arr, low, high);
-			System.out.println(
-					"pivot : " + pi + " " + "low : " + low + " " + "high : " + high + " " + Arrays.toString(arr));
+			int pi = partition(arr, low, high); // pivot element is in the right place after partition
 
-			// Recursively sort elements before
-			// partition and after partition
-			System.out.println("sort left");
-			quickSort(arr, low, pi - 1);
-			System.out.println("sort right");
-			quickSort(arr, pi + 1, high);
+			quickSort(arr, low, pi - 1); // sort left elements of pivot
+			quickSort(arr, pi + 1, high); // sort right elements of pivot
 		}
 	}
+
 	public static void main(String[] args) {
 		int[] arr = { 4, 2, 9, 6, 23, 12, 34, 0, 1 };
 		quickSort(arr, 0, arr.length - 1);

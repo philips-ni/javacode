ec8bbc6425a5f05f1705b6f9a657bb61305d3f7d
wli118 <13264444+wli118@users.noreply.github.com>
2018-05-25 13:42:41 -0700

    binary search

--- util/BinarySearch.java
+++ util/BinarySearch.java
@@ -26,12 +26,27 @@ public class BinarySearch {
 
 		return -1;
 	}
+
+	public static int binarySearch_recursive(int[] nums, int target, int low, int high) {
+		if (low > high)
+			return -1;
+
+		int mid = (low + high) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		} else if (nums[mid] > target) {
+			return binarySearch_recursive(nums, target, low, mid - 1);
+		} else {
+			return binarySearch_recursive(nums, target, mid + 1, high);
+		}
+	}
+
 	public static void main(String[] args) {
 
 		int[] SA = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
 		System.out.println("Input  : " + Arrays.toString(SA));
-		System.out.println("Output : " + binarySearch(SA, 9));
-
+		System.out.println("Output : " + binarySearch(SA, 6));
+		System.out.println("Output : " + binarySearch_recursive(SA, 6, 0, SA.length - 1));
 
 	}
 

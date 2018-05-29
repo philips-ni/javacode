8ebc8aed471ae08c8817588ae3e18e85c8c133a2
wli118 <liw0118@gmail.com>
2018-05-28 23:27:14 -0700

    linkedlist

--- util/LinkedList.java
+++ util/LinkedList.java
@@ -1,12 +1,10 @@
 package util;
 
 /**
- *
- * Singly Linked Lists are a type of data structure. It is a type of list. In a
- * singly linked list each node in the list stores the contents of the node and
- * a pointer or reference to the next node in the list. It does not store any
- * pointer or reference to the previous node. It is called a singly linked list
- * because each node only has a single link to another node.
+ * In a singly linked list, each node in the list stores the contents of the
+ * node and a pointer or reference to the next node in the list. It does not
+ * store any pointer or reference to the previous node. It is called a singly
+ * linked list because each node only has a single link to another node.
  * 
  * To store a single linked list, you only need to store a reference or pointer
  * to the first node in that list. The last node has a pointer to nothingness to
@@ -16,9 +14,7 @@ package util;
 public class LinkedList {
 
 	private LinkedListNode head;
-	private LinkedListNode tail; // using tail pointer is much easier to add
-									// node to the
-	// end
+	private LinkedListNode tail; // easier to add node to the end with tail
 
 	private int count;
 

66ebb356c680f1b211890cc177cc06d0cd319ed0
wli118 <13264444+wli118@users.noreply.github.com>
2018-06-05 16:25:35 -0700

    longest substring

--- leetcode/LC_003_Longest_Substring.java
+++ leetcode/LC_003_Longest_Substring.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * LC.String.003 Longest Substring Without Repeating Characters
@@ -21,31 +23,25 @@ import java.util.HashMap;
 public class LC_003_Longest_Substring {
 
 	public static int lengthOfLongestSubstring(String s) {
-		if (s.length() == 0)
-			return 0;
-
-		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-		int max = 0;
-		for (int left = 0, right = 0; right < s.length(); right++) {
-
-			// found duplicate character and reset left pointer
-			if (map.containsKey(s.charAt(right))) {
-				left = map.get(s.charAt(right)) + 1;
+		int n = s.length();
+		Set<Character> set = new HashSet<>();
+		int max = 0, i = 0, j = 0;
+		while (i < n && j < n) {
+			if (!set.contains(s.charAt(j))) {
+				// find max in range[i,j]
+				set.add(s.charAt(j++));
+				max = Math.max(max, j - i);
+			} else {
+				// remove left from set and move left to next char
+				set.remove(s.charAt(i++));
 			}
-
-			// put or update the right character
-			map.put(s.charAt(right), right);
-
-			// get longest string length
-			max = Math.max(max, right - left + 1);
 		}
 		return max;
 	}
 
 	public static void main(String[] args) {
-		String s = new String("abcabcdabc");	
+		String s = new String("abba");
 		System.out.println("Input  : " + s);
 		System.out.println("Output : " + lengthOfLongestSubstring(s));
 	}
-
 }

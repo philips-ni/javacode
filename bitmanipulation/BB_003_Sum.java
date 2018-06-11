8f3077d53800db715e7e311817321e2351a96ed3
wli118 <13264444+wli118@users.noreply.github.com>
2018-06-10 22:59:24 -0700

    sum with bit manipulation

--- bitmanipulation/BB_003_Sum.java
+++ bitmanipulation/BB_003_Sum.java
@@ -3,8 +3,8 @@ package bitmanipulation;
 /**
  * BB.BitManipulation.003 Sum
  * 
- * Question : Given two integers, write a function to sum the numbers
- * without using any arithmetic operators.
+ * Question : Given two integers, write a function to sum the numbers without
+ * using any arithmetic operators.
  * 
  * e.g. sum(5+3) = 8
  */
@@ -13,13 +13,27 @@ public class BB_003_Sum {
 	public static int sum(int a, int b) {
 		if (b == 0)
 			return a;
+		int sum = a;
+		while (b != 0) {
+			sum = a ^ b; // partial sum without carry bits
+			b = (a & b) << 1; // carry bits
+			a = sum; // save partial sum in a for next loop
+		}
+		return sum;
+	}
+
+	public static int sum_recur(int a, int b) {
+		if (b == 0)
+			return a;
 		int partialSum = a ^ b;
 		int carry = (a & b) << 1;
-		return sum(partialSum, carry);
+		return sum_recur(partialSum, carry);
 	}
 
 	public static void main(String[] args) {
-		
 		System.out.println(sum(5, 3));
+		System.out.println(sum_recur(5, 3));
+		System.out.println(sum(-5, 3));
+		System.out.println(sum_recur(-5, 3));
 	}
 }

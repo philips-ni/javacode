7d45d8a62dc51efa506652791582adb55426890e
wli118 <13264444+wli118@users.noreply.github.com>
2018-06-13 11:26:15 -0700

    make changes

--- dynamicprogramming/BB_007_Make_Change.java
+++ dynamicprogramming/BB_007_Make_Change.java
@@ -1,5 +1,7 @@
 package dynamicprogramming;
 
+import java.util.Arrays;
+
 /**
  * BB.DP.007 Make change
  * 
@@ -51,10 +53,38 @@ public class BB_007_Make_Change {
 		return makeChanges(coins, num - 1, change) + makeChanges(coins, num, change - coins[num - 1]);
 	}
 
+	public static long makeChanges_bottomup(int coins[], int num, int change)
+    {
+        //Time complexity of this function: O(mn)
+        //Space Complexity of this function: O(n)
+ 
+        // table[i] will be storing the number of solutions
+        // for value i. We need n+1 rows as the table is
+        // constructed in bottom up manner using the base
+        // case (n = 0)
+        long[] table = new long[change+1];
+ 
+        // Initialize all table values as 0
+		Arrays.fill(table, 0);   //O(n)
+ 
+        // Base case (If given value is 0)
+        table[0] = 1;
+ 
+        // Pick all coins one by one and update the table[]
+        // values after the index greater than or equal to
+        // the value of the picked coin
+        for (int i=0; i<num; i++)
+            for (int j=coins[i]; j<=change; j++)
+                table[j] += table[j-coins[i]];
+ 
+        return table[change];
+    }
+	
 	// Driver program to test above function
 	public static void main(String[] args) {
 		int coins[] = { 1, 5, 10, 25 };
 		int num = coins.length;
 		System.out.println(makeChanges(coins, num, 25));
+		System.out.println(makeChanges_bottomup(coins, num, 25));
 	}
 }

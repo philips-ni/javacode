fc7a2b63a2c2d79f6d049a921d24fd008cd04ee0
wli118 <liw0118@gmail.com>
2018-06-23 22:13:04 -0700

    gray code

--- bitmanipulation/BB_005_Gray_Code.java
+++ bitmanipulation/BB_005_Gray_Code.java
@@ -15,6 +15,28 @@ package bitmanipulation;
  */
 public class BB_005_Gray_Code {
 
+	public static boolean grayCode_bf(int a, int b) {
+		if (a == b)
+			return false; // all bits are equal
+
+		int count = 0; // count number of different bits
+
+		while (a != 0 || b != 0) {
+			int lsb_a = a & 1; // get last bit of a
+			int lsb_b = b & 1; // get last bit of b
+			if ((lsb_a ^ lsb_b) == 1) {  // check if last bit of a and b is equal or not
+				count++;
+			}
+			if (count > 1)
+				return false;
+			else {
+				a >>= 1;
+				b >>= 1;
+			}
+		}
+		return true;
+	}
+
 	public static boolean grayCode(int a, int b) {
 		int x = a ^ b;
 
@@ -32,12 +54,16 @@ public class BB_005_Gray_Code {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(grayCode(0,1));
-		System.out.println(grayCode(2,1));
-		
-		System.out.println(grayCode_1(0,1));
-		System.out.println(grayCode_1(2,1));
+
+		System.out.println(grayCode_bf(0, 1));
+		System.out.println(grayCode_bf(0, 9));
+		System.out.println(grayCode_bf(2, 1));
+
+		System.out.println(grayCode(0, 1));
+		System.out.println(grayCode(2, 1));
+
+		System.out.println(grayCode_1(0, 1));
+		System.out.println(grayCode_1(2, 1));
 
 	}
-
 }
